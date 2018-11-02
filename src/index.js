b448b60c3d011a943d9cf568139f8242eb44466d
Crysisun <sunqingyu1@gmail.com>
2018-11-01 23:53:41 -0700

    Creates root index of router.

--- src/index.js
+++ src/index.js
@@ -4,30 +4,18 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import { BrowserRouter, Route } from 'react-router-dom';
+import PostsIndex from './components/post_index';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
-class Hello extends React.Component {
-    render() {
-        return <div>hello</div>
-    }
-}
-class Goodbye extends React.Component {
-    render() {
-        return <div>goodbye</div>
-    }
-}
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         {/*<App />*/}
         <BrowserRouter>
             <div>
-            <Route path="/hello" component={Hello} />
-            <Route path="/goodbye" component={Goodbye} />
+                <Route path="/" component={PostsIndex}/>
             </div>
         </BrowserRouter>
     </Provider>
