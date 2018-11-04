24d444d9bb1c3ccd36eca05d9ed0c79477cc4bac
Crysisun <sunqingyu1@gmail.com>
2018-11-03 18:38:51 -0700

    Implementing redux form.

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-    render() {
+    renderTitleField(field) {
         return (
             <div>
-                YEEE
+                <input
+                    type="text"
+                    {...field.input}
+                />
             </div>
+        );
+    }
+    render() {
+        return (
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
