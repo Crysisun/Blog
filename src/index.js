f09f741c3badd7d2281901bb39524d99282f622b
Crysisun <sunqingyu1@gmail.com>
2018-11-05 17:12:50 -0800

    Implementing posts_show component in order to show single selected post.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PostsIndex from './components/post_index';
 import PostsNew from './components/post_new';
 import reducers from './reducers';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
@@ -18,6 +19,7 @@ ReactDOM.render(
             <div>
                 <Switch>
                     <Route path="/posts/new" component={PostsNew}/>
+                    <Route path="/posts/:id" component={PostsShow}/>
                     <Route path="/" component={PostsIndex}/>
                 </Switch>
             </div>
--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action) { //default state to be a null object
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            return { ...state, [action.payload.data.id]: action.payload.data };
         default:
             return state;
     }
