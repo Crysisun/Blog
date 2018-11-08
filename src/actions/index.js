f3f21da09c1ec3a655e65d68d2ece2559da1d6ec
Crysisun <sunqingyu1@gmail.com>
2018-11-08 00:22:55 -0800

    Makes optimization on the delete action by using omit function from lodash, it can help delete the specific one directly from the original array without the need of getting all the data from backend again.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
-export const CREATE_POST = 'CREATE_POST';
-export const FETCH_POST = 'FETCH_POST';
-export const DELETE_POST = 'DELETE_POST';
+export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=sun1234';
@@ -36,8 +36,9 @@ export function fetchPost(id) {
     };
 }
 
-export function deletePost(id) {
-    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
 
     return {
         type: DELETE_POST,
--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions/index';
 
 export default function(state = {}, action) { //default state to be a null object
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
         case FETCH_POST:
